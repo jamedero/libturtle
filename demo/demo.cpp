322689b9e15b063e5cf86dd7e65cb4010540dc21
KeyronLinarez <keyronlinarez@gmail.com>
2023-11-09 23:17:57 -0800

    Added a polygon function.

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -9,13 +9,26 @@ using namespace std;
 
 void rect(Turtle& t, float width, float height) {
   t.forward(width);
-  // t.right(90);
-  // t.forward(height);
-  // t.right(90);
-  // t.forward(width);
-  // t.right(90);
-  // t.forward(height);
-  // t.right(90);
+  t.right(90);
+  t.forward(height);
+  t.right(90);
+  t.forward(width);
+  t.right(90);
+  t.forward(height);
+  t.right(90);
+}
+
+void poly(Turtle& t, float size, int sides) {
+
+  if (sides < 3){
+    std::cout << "Polygon  must be at least 3 sides";
+  } else{
+    for (int x = 0; x < sides; ++x){
+      t.forward(size);
+      degree = 360.0 / sides;
+      t.left(degree);
+    }
+  }
 }
 
 
@@ -23,7 +36,8 @@ void meetTurtle() {
   Turtle t;
   t.satinon(0.3);
   t.pendown();  
-  rect(t, 20, 30);
+  //rect(t, 20, 30);
+  poly(t, 20, 3);
   t.end();
   t.save("demo.dst");
 }
