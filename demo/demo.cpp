bbbe3dafac03db335c15f1998e16b0ffb8cf95fc
Cole Lameyer <farne@localhost.localdomain>
2023-11-12 15:15:29 -0800

    Added curving

--- demo/demo.cpp
+++ demo/demo.cpp
@@ -18,14 +18,31 @@ void rect(Turtle& t, float width, float height) {
   t.right(90);
 }
 
+void softrect(Turtle& t, float width, float height) {
+  t.forward(width);
+  t.rcurve(90, 10);
+  t.forward(height);
+  t.rcurve(90, 10);
+  t.forward(width);
+  t.rcurve(90, 10);
+  t.forward(height);
+  t.rcurve(90, 10);
+}
 
 void meetTurtle() {
-  Turtle t;
-  t.satinon(0.3);
-  t.pendown();  
-  rect(t, 20, 30);
-  t.end();
-  t.save("demo.dst");
+  Turtle rt;
+  rt.satinon(0.3);
+  rt.pendown();  
+  rect(rt, 20, 30);
+  rt.end();
+  rt.save("demo_rect.dst");
+
+  Turtle srt;
+  srt.satinon(0.3);
+  srt.pendown();  
+  softrect(srt, 20, 30);
+  srt.end();
+  srt.save("demo_softrect.dst");
 }
 
 int main() {
